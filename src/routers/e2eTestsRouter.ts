8786138447c0a0c9767857a505b7222e24df4a1b
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-05-23 05:09:18 -0300

    Preparando revisão de código

--- src/routers/e2eTestsRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import e2eTestsController from "../controllers/e2eTestsController.js";
-
-const e2eRouter = Router();
-
-e2eRouter.post("/reset", e2eTestsController.reset);
-e2eRouter.post("/seed", e2eTestsController.seed);
-
-export default e2eRouter;
